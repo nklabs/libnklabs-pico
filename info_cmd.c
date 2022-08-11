642b8d7a574f308ed5f6506d9907f650054943d2
Joe Allen <jhallenworld@gmail.com>
2022-08-11 19:06:09 -0400

    More info

--- info_cmd.c
+++ info_cmd.c
@@ -26,6 +26,7 @@
 #include "nkcli.h"
 #include "pico/unique_id.h"
 #include "hardware/watchdog.h"
+#include "hardware/clocks.h"
 
 // Defines from linker
 extern char __boot2_start__;
@@ -39,6 +40,28 @@ extern char __bss_start__;
 extern char __bss_end__;
 extern char __end__; // Heap
 extern char __StackLimit; // End of RAM
+
+void measure_freqs(void) {
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+ 
+    nk_printf("pll_sys  = %dkHz\n", f_pll_sys);
+    nk_printf("pll_usb  = %dkHz\n", f_pll_usb);
+    nk_printf("rosc     = %dkHz\n", f_rosc);
+    nk_printf("clk_sys  = %dkHz\n", f_clk_sys);
+    nk_printf("clk_peri = %dkHz\n", f_clk_peri);
+    nk_printf("clk_usb  = %dkHz\n", f_clk_usb);
+    nk_printf("clk_adc  = %dkHz\n", f_clk_adc);
+    nk_printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+ 
+    // Can't measure clk_ref / xosc as it is the ref
+}
 
 // Print information about firmware and system
 
@@ -56,6 +79,8 @@ static int cmd_info(nkinfile_t *args)
 		nk_printf("  sizeof(long) = %u\n", sizeof(long));
 		nk_printf("  sizeof(long long) = %u\n", sizeof(long long));
 		nk_printf("  sizeof(void *) = %u\n", sizeof(void *));
+		nk_printf("  sizeof(float) = %u\n", sizeof(float));
+		nk_printf("  sizeof(double) = %u\n", sizeof(double));
 		pico_unique_board_id_t board_id;
 		pico_get_unique_board_id(&board_id);
 
@@ -65,22 +90,22 @@ static int cmd_info(nkinfile_t *args)
 		}
 		nk_printf("\n");
 
+		nk_printf("Flash locations:\n");
 		nk_printf("  __boot2_start__ = %p\n", &__boot2_start__);
 		nk_printf("  __boot2_end__ = %p\n", &__boot2_end__);
 		nk_printf("  __logical_binary_start = %p\n", &__logical_binary_start);
 		nk_printf("  __etext = %p\n", &__etext);
 		nk_printf("  __flash_binary_end = %p\n", &__flash_binary_end);
+		nk_printf("  an address in current text = %p\n", &cmd_info);
 
+		nk_printf("RAM locations:\n");
 		nk_printf("  __data_start__ = %p\n", &__data_start__);
 		nk_printf("  __data_end__ = %p\n", &__data_end__);
 		nk_printf("  __bss_start__ = %p\n", &__bss_start__);
 		nk_printf("  __bss_end__ = %p\n", &__bss_end__);
 		nk_printf("  __end__ = %p\n", &__end__);
 		nk_printf("  __StackLimit = %p\n", &__StackLimit);
-
 		nk_printf("  an address in current stack = %p\n", &args);
-		nk_printf("  an address in current text = %p\n", &cmd_info);
-
 		nk_printf("RAM size = %u\n", (unsigned int)&__StackLimit - 0x20000000);
 
 		const char *cause;
@@ -92,6 +117,7 @@ static int cmd_info(nkinfile_t *args)
 			cause = "Hardware";
 		nk_printf("Reboot cause = %s\n", cause);
 
+		measure_freqs();
 	} else {
 		nk_printf("Syntax error\n");
 	}
